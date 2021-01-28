2a2ec4149034077680d1b3dc16e3834d22a2ffc1
gabid <gabriela.netor94@gmail.com>
2021-01-28 00:26:44 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state={
+      titulo: 'Cadastro de Produtos',
+      acao: 0,
+      index: '',
+      dados: []
+    }
+  }
+
+  render() {
+    let dados = this.state.dados;
+    return (
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <form ref="formulario" className="formulario">
+          <input type="text" ref="name" placeholder="Produto" className="campoForm" />
+          <input type="text" ref="address" placeholder="Preço" className="campoForm" />
+          <button  className="botaoForm">Buscar </button>
+        </form>
+      </div>
+    );  
+  }
 }
 
 export default App;
